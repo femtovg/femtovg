141b1c079174b04de85e4153a6795bbe876ad9b0
Simon Hausmann <hausmann@gmail.com>
2020-12-18 08:05:28 +0100

    Fix stencil buffer attachment on FBOs with WebGL 1
    DEPTH24_STENCIL8 is not supported in WebGL 1. The original nanovg code
    uses STENCIL_INDEX8 but that's not working with Safari/Chrome for
    mysterious reasons.
    
    However WebGL 1 adds the unsized DEPTH_STENCIL as a workaround (that's
    not in GLES 2.0):
    
        https://www.khronos.org/registry/webgl/specs/latest/1.0/#6.8
    
    So fall back to that if DEPTH24_STENCIL8 doesn't work.

--- src/renderer/opengl/framebuffer.rs
+++ src/renderer/opengl/framebuffer.rs
@@ -22,60 +22,85 @@ impl Framebuffer {
         let width = texture.info().width() as u32;
         let height = texture.info().height() as u32;
 
+        let attach_texture = {
+            || unsafe {
+                context.framebuffer_texture_2d(
+                    glow::FRAMEBUFFER,
+                    glow::COLOR_ATTACHMENT0,
+                    glow::TEXTURE_2D,
+                    Some(texture.id()),
+                    0,
+                );
+            }
+        };
+
+        attach_texture();
+
+        let depth_stencil_rbo = unsafe { context.create_renderbuffer().unwrap() };
         unsafe {
-            context.framebuffer_texture_2d(
-                glow::FRAMEBUFFER,
-                glow::COLOR_ATTACHMENT0,
-                glow::TEXTURE_2D,
-                Some(texture.id()),
-                0,
-            );
+            context.bind_renderbuffer(glow::RENDERBUFFER, Some(depth_stencil_rbo));
+            context.renderbuffer_storage(glow::RENDERBUFFER, glow::DEPTH24_STENCIL8, width as i32, height as i32);
+            context.bind_renderbuffer(glow::RENDERBUFFER, None);
         }
 
-        let depth_stencil_rbo = Self::gen_depth_stencil_rbo(context, width, height);
-
-        let fbo = Framebuffer {
-            context: context.clone(),
-            fbo,
-            depth_stencil_rbo,
+        let attach_stencil_rbo = {
+            || unsafe {
+                context.framebuffer_renderbuffer(
+                    glow::FRAMEBUFFER,
+                    glow::DEPTH_STENCIL_ATTACHMENT,
+                    glow::RENDERBUFFER,
+                    Some(depth_stencil_rbo),
+                );
+            }
         };
 
         unsafe {
-            context.framebuffer_renderbuffer(
-                glow::FRAMEBUFFER,
-                glow::DEPTH_STENCIL_ATTACHMENT,
-                glow::RENDERBUFFER,
-                Some(depth_stencil_rbo),
-            );
+            attach_stencil_rbo();
 
             let status = context.check_framebuffer_status(glow::FRAMEBUFFER);
 
             if status != glow::FRAMEBUFFER_COMPLETE {
-                let reason = match status {
-                    glow::FRAMEBUFFER_INCOMPLETE_ATTACHMENT => {
-                        format!("({}) Framebuffer incomplete attachment", status)
-                    }
-                    //glow::FRAMEBUFFER_INCOMPLETE_DRAW_BUFFER => format!("({}) Framebuffer incomplete draw buffer", status),
-                    //glow::FRAMEBUFFER_INCOMPLETE_LAYER_TARGETS => format!("({}) Framebuffer incomplete layer targets", status),
-                    //FIXME: will be in next glow release: glow::FRAMEBUFFER_INCOMPLETE_DIMENSIONS => format!("({}) Framebuffer incomplete dimensions", status),
-                    glow::FRAMEBUFFER_INCOMPLETE_MISSING_ATTACHMENT => {
-                        format!("({}) Framebuffer incomplete missing attachment", status)
-                    }
-                    glow::FRAMEBUFFER_INCOMPLETE_MULTISAMPLE => {
-                        format!("({}) Framebuffer incomplete multisample", status)
-                    }
-                    //glow::FRAMEBUFFER_INCOMPLETE_READ_BUFFER => format!("({}) Framebuffer incomplete read buffer", status),
-                    glow::FRAMEBUFFER_UNSUPPORTED => format!("({}) Framebuffer unsupported", status),
-                    _ => format!("({}) Framebuffer not complete!", status),
-                };
+                // DEPTH24_STENCIL8 is not supported in WebGL 1, so fall back to the unsized DEPTH_STENCIL, a workaround
+                // that's in the WebGL 1.0 spec.
+                context.bind_renderbuffer(glow::RENDERBUFFER, Some(depth_stencil_rbo));
+                context.renderbuffer_storage(glow::RENDERBUFFER, glow::DEPTH_STENCIL, width as i32, height as i32);
+                context.bind_renderbuffer(glow::RENDERBUFFER, None);
+                attach_texture();
+                attach_stencil_rbo();
 
-                return Err(ErrorKind::RenderTargetError(reason));
+                let status = context.check_framebuffer_status(glow::FRAMEBUFFER);
+
+                if status != glow::FRAMEBUFFER_COMPLETE {
+                    let reason = match status {
+                        glow::FRAMEBUFFER_INCOMPLETE_ATTACHMENT => {
+                            format!("({}) Framebuffer incomplete attachment", status)
+                        }
+                        //glow::FRAMEBUFFER_INCOMPLETE_DRAW_BUFFER => format!("({}) Framebuffer incomplete draw buffer", status),
+                        //glow::FRAMEBUFFER_INCOMPLETE_LAYER_TARGETS => format!("({}) Framebuffer incomplete layer targets", status),
+                        //FIXME: will be in next glow release: glow::FRAMEBUFFER_INCOMPLETE_DIMENSIONS => format!("({}) Framebuffer incomplete dimensions", status),
+                        glow::FRAMEBUFFER_INCOMPLETE_MISSING_ATTACHMENT => {
+                            format!("({}) Framebuffer incomplete missing attachment", status)
+                        }
+                        glow::FRAMEBUFFER_INCOMPLETE_MULTISAMPLE => {
+                            format!("({}) Framebuffer incomplete multisample", status)
+                        }
+                        //glow::FRAMEBUFFER_INCOMPLETE_READ_BUFFER => format!("({}) Framebuffer incomplete read buffer", status),
+                        glow::FRAMEBUFFER_UNSUPPORTED => format!("({}) Framebuffer unsupported", status),
+                        _ => format!("({}) Framebuffer not complete!", status),
+                    };
+
+                    return Err(ErrorKind::RenderTargetError(reason));
+                }
             }
 
             context.bind_framebuffer(glow::FRAMEBUFFER, None);
         }
 
-        Ok(fbo)
+        Ok(Framebuffer {
+            context: context.clone(),
+            fbo,
+            depth_stencil_rbo,
+        })
     }
 
     pub fn bind(&self) {
@@ -114,20 +139,6 @@ impl Framebuffer {
     //         glow::BindFramebuffer(glow::DRAW_FRAMEBUFFER, 0);
     //     }
     // }
-
-    fn gen_depth_stencil_rbo(
-        context: &Rc<glow::Context>,
-        width: u32,
-        height: u32,
-    ) -> <glow::Context as glow::HasContext>::Renderbuffer {
-        unsafe {
-            let id = context.create_renderbuffer().unwrap();
-            context.bind_renderbuffer(glow::RENDERBUFFER, Some(id));
-            context.renderbuffer_storage(glow::RENDERBUFFER, glow::DEPTH24_STENCIL8, width as i32, height as i32);
-            context.bind_renderbuffer(glow::RENDERBUFFER, None);
-            id
-        }
-    }
 }
 
 impl Drop for Framebuffer {
