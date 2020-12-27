c3a7dd3368a0d0deab1129b8253a6c027ba9e0e2
adamnemecek <adamnemecek@gmail.com>
2020-12-27 09:53:33 -0800

    working on wgpu

--- src/renderer/webgpu.rs
+++ src/renderer/webgpu.rs
@@ -22,6 +22,43 @@ pub struct GPUCommandEncoder {
     rps: wgpu::RenderPipeline
 }
 // pu bn 
+#[derive(Copy, Clone, PartialEq, Eq, Debug, Hash)]
+pub struct WGBlend {
+    pub src_rgb: wgpu::BlendFactor,
+    pub dst_rgb: wgpu::BlendFactor,
+    pub src_alpha: wgpu::BlendFactor,
+    pub dst_alpha: wgpu::BlendFactor,
+}
+
+
+impl From<BlendFactor> for wgpu::BlendFactor {
+    fn from(a: BlendFactor) -> Self {
+        match a {
+            BlendFactor::Zero => Self::Zero,
+            BlendFactor::One => Self::One,
+            BlendFactor::SrcColor => Self::SrcColor,
+            BlendFactor::OneMinusSrcColor => Self::OneMinusSrcColor,
+            BlendFactor::DstColor => Self::DstColor,
+            BlendFactor::OneMinusDstColor => Self::OneMinusDstColor,
+            BlendFactor::SrcAlpha => Self::SrcAlpha,
+            BlendFactor::OneMinusSrcAlpha => Self::OneMinusSrcAlpha,
+            BlendFactor::DstAlpha => Self::DstAlpha,
+            BlendFactor::OneMinusDstAlpha => Self::OneMinusDstAlpha,
+            BlendFactor::SrcAlphaSaturate => Self::SrcAlphaSaturated,
+        }
+    }
+}
+
+impl From<CompositeOperationState> for WGBlend {
+    fn from(v: CompositeOperationState) -> Self {
+        Self {
+            src_rgb: v.src_rgb.into(),
+            dst_rgb: v.dst_rgb.into(),
+            src_alpha: v.src_alpha.into(),
+            dst_alpha: v.dst_alpha.into(),
+        }
+    }
+}
 
 pub struct WebGPU {
     device: wgpu::Device,
@@ -38,42 +75,3 @@ impl WebGPU {
 
 }
 
-#[derive(Copy, Clone, PartialEq, Eq, Debug, Hash)]
-pub struct WGBlend {
-    pub src_rgb: wgpu::BlendFactor,
-    pub dst_rgb: wgpu::BlendFactor,
-    pub src_alpha: wgpu::BlendFactor,
-    pub dst_alpha: wgpu::BlendFactor,
-}
-
-// Zero = 0,
-// One = 1,
-// SrcColor = 2,
-// OneMinusSrcColor = 3,
-// SrcAlpha = 4,
-// OneMinusSrcAlpha = 5,
-// DstColor = 6,
-// OneMinusDstColor = 7,
-// DstAlpha = 8,
-// OneMinusDstAlpha = 9,
-// SrcAlphaSaturated = 10,
-// BlendColor = 11,
-// OneMinusBlendColor = 12,
-
-// impl From<BlendFactor> for wgpu::BlendFactor {
-//     fn from(a: BlendFactor) -> Self {
-//         match a {
-//             BlendFactor::Zero => Self::Zero,
-//             BlendFactor::One => Self::One,
-//             BlendFactor::SrcColor => Self::SrcColor,
-//             BlendFactor::OneMinusSrcColor => Self::OneMinusSrcColor,
-//             BlendFactor::DstColor => Self::DestinationColor,
-//             BlendFactor::OneMinusDstColor => Self::OneMinusDestinationColor,
-//             BlendFactor::SrcAlpha => Self::SourceAlpha,
-//             BlendFactor::OneMinusSrcAlpha => Self::OneMinusSourceAlpha,
-//             BlendFactor::DstAlpha => Self::DestinationAlpha,
-//             BlendFactor::OneMinusDstAlpha => Self::OneMinusDestinationAlpha,
-//             BlendFactor::SrcAlphaSaturate => Self::SourceAlphaSaturated,
-//         }
-//     }
-// }
