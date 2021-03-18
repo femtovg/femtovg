bb86513ced7459e9836eca3ee551a64cbd33cae6
adamnemecek <adamnemecek@gmail.com>
2021-03-18 07:49:33 -0700

    wgpu_texture

--- src/renderer/webgpu/wgpu_texture.rs
+++ src/renderer/webgpu/wgpu_texture.rs
@@ -21,7 +21,7 @@ impl From<PixelFormat> for wgpu::TextureFormat {
 pub struct WGPUTexture {
     //
     info: ImageInfo,
-    tex: wgpu::Texture,
+    tex: wgpu::TextureView,
     sampler: wgpu::Sampler,
     context: WGPUContext,
 }
@@ -32,7 +32,6 @@ impl WGPUTexture {
     }
 
     pub fn new(context: &WGPUContext, info: ImageInfo) -> Self {
-
         assert!(info.format() != PixelFormat::Rgb8);
         let context = context.clone();
 
@@ -43,21 +42,63 @@ impl WGPUTexture {
 
         let format = info.format().into();
 
-        let texture_view = context.device()
-        .create_texture(&wgpu::TextureDescriptor {
-            label: Some("Low Resolution Target"),
-            size: wgpu::Extent3d {
-                width: 0,
-                height: 0,
-                depth_or_array_layers: 1,
-            },
-            mip_level_count: 1,
-            sample_count: 1,
-            dimension: wgpu::TextureDimension::D2,
-            format,
-            usage: wgpu::TextureUsage::SAMPLED | wgpu::TextureUsage::RENDER_ATTACHMENT,
-        })
-        .create_view(&Default::default());
-        todo!()
+        let mip_level_count = if generate_mipmaps { 0 } else { 0 };
+
+        // todo: what's the difference between texture and texture_view
+        let tex = context
+            .device()
+            .create_texture(&wgpu::TextureDescriptor {
+                label: Some("Low Resolution Target"),
+                size: wgpu::Extent3d {
+                    width: 0,
+                    height: 0,
+                    depth_or_array_layers: 1,
+                },
+                mip_level_count,
+                sample_count: 1,
+                dimension: wgpu::TextureDimension::D2,
+                format,
+                //todo!
+                usage: wgpu::TextureUsage::SAMPLED | wgpu::TextureUsage::RENDER_ATTACHMENT,
+            })
+            .create_view(&Default::default());
+
+        let filter = if nearest {
+            wgpu::FilterMode::Nearest
+        } else {
+            wgpu::FilterMode::Linear
+        };
+
+        let mut sampler_desc = wgpu::SamplerDescriptor {
+            label: Some("Nearest Neighbor Sampler"),
+            mag_filter: filter,
+            min_filter: filter,
+            ..Default::default()
+        };
+
+        if generate_mipmaps {
+            sampler_desc.mipmap_filter = filter;
+        }
+
+        sampler_desc.address_mode_u = if repeatx {
+            wgpu::AddressMode::Repeat
+        } else {
+            wgpu::AddressMode::ClampToEdge
+        };
+
+        sampler_desc.address_mode_v = if repeaty {
+            wgpu::AddressMode::Repeat
+        } else {
+            wgpu::AddressMode::ClampToEdge
+        };
+
+        let sampler = context.device().create_sampler(&sampler_desc);
+
+        Self {
+            sampler,
+            context,
+            info,
+            tex,
+        }
     }
 }
