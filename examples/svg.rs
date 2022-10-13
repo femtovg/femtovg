1b637675e282e14d519d8714dc0670ae04a67e3f
Fabio U. Krapohl <Krapohl.f@gmx.de>
2022-10-14 01:07:46 +0200

    Svg example cleanup

--- examples/svg.rs
+++ examples/svg.rs
@@ -48,7 +48,7 @@ fn run(
     let mut perf = PerfGraph::new();
 
     let svg_data = include_str!("assets/Ghostscript_Tiger.svg").as_bytes();
-    let tree = usvg::Tree::from_data(&svg_data, &usvg::Options::default().to_ref()).unwrap();
+    let tree = usvg::Tree::from_data(svg_data, &usvg::Options::default().to_ref()).unwrap();
 
     let mut paths = render_svg(tree);
 
@@ -186,43 +186,40 @@ fn render_svg(svg: usvg::Tree) -> Vec<(Path, Option<Paint>, Option<Paint>)> {
     let mut paths = Vec::new();
 
     for node in svg.root().descendants() {
-        match &*node.borrow() {
-            NodeKind::Path(svg_path) => {
-                let mut path = Path::new();
+        if let NodeKind::Path(svg_path) = &*node.borrow() {
+            let mut path = Path::new();
 
-                for command in svg_path.data.iter() {
-                    match command {
-                        PathSegment::MoveTo { x, y } => path.move_to(*x as f32, *y as f32),
-                        PathSegment::LineTo { x, y } => path.line_to(*x as f32, *y as f32),
-                        PathSegment::CurveTo { x1, y1, x2, y2, x, y } => {
-                            path.bezier_to(*x1 as f32, *y1 as f32, *x2 as f32, *y2 as f32, *x as f32, *y as f32)
-                        }
-                        PathSegment::ClosePath => path.close(),
+            for command in svg_path.data.iter() {
+                match command {
+                    PathSegment::MoveTo { x, y } => path.move_to(*x as f32, *y as f32),
+                    PathSegment::LineTo { x, y } => path.line_to(*x as f32, *y as f32),
+                    PathSegment::CurveTo { x1, y1, x2, y2, x, y } => {
+                        path.bezier_to(*x1 as f32, *y1 as f32, *x2 as f32, *y2 as f32, *x as f32, *y as f32)
                     }
+                    PathSegment::ClosePath => path.close(),
                 }
-
-                let to_femto_color = |usvg_paint: &usvg::Paint| match usvg_paint {
-                    usvg::Paint::Color(usvg::Color { red, green, blue }) => Some(Color::rgb(*red, *green, *blue)),
-                    _ => None,
-                };
-
-                let fill = svg_path
-                    .fill
-                    .as_ref()
-                    .and_then(|fill| to_femto_color(&fill.paint))
-                    .map(Paint::color);
-
-                let stroke = svg_path.stroke.as_ref().and_then(|stroke| {
-                    to_femto_color(&stroke.paint).map(|paint| {
-                        let mut stroke_paint = Paint::color(paint);
-                        stroke_paint.set_line_width(stroke.width.value() as f32);
-                        stroke_paint
-                    })
-                });
-
-                paths.push((path, fill, stroke))
             }
-            _ => (),
+
+            let to_femto_color = |usvg_paint: &usvg::Paint| match usvg_paint {
+                usvg::Paint::Color(usvg::Color { red, green, blue }) => Some(Color::rgb(*red, *green, *blue)),
+                _ => None,
+            };
+
+            let fill = svg_path
+                .fill
+                .as_ref()
+                .and_then(|fill| to_femto_color(&fill.paint))
+                .map(Paint::color);
+
+            let stroke = svg_path.stroke.as_ref().and_then(|stroke| {
+                to_femto_color(&stroke.paint).map(|paint| {
+                    let mut stroke_paint = Paint::color(paint);
+                    stroke_paint.set_line_width(stroke.width.value() as f32);
+                    stroke_paint
+                })
+            });
+
+            paths.push((path, fill, stroke))
         }
     }
 
