8d107044fce42e7bb7517f4d24c6a377d97a3549
Audrey Dutcher <audrey@rhelmot.io>
2023-01-16 09:57:05 -0600

    Don't use let-else :(

--- examples/external_text.rs
+++ examples/external_text.rs
@@ -74,7 +74,7 @@ impl RenderCache {
                 cache_key.x_bin = subpixel_x;
                 cache_key.y_bin = subpixel_y;
                 // perform cache lookup for rendered glyph
-                let Some(rendered) = self.rendered_glyphs.entry(cache_key).or_insert_with(|| {
+                if let Some(rendered) = self.rendered_glyphs.entry(cache_key).or_insert_with(|| {
                     // ...or insert it
 
                     // do the actual rasterization
@@ -156,33 +156,33 @@ impl RenderCache {
                             color_glyph: matches!(rendered.content, Content::Color),
                         }
                     })
-                }) else { continue };
+                }) {
+                    let cmd_map = if rendered.color_glyph {
+                        &mut color_cmd_map
+                    } else {
+                        &mut alpha_cmd_map
+                    };
 
-                let cmd_map = if rendered.color_glyph {
-                    &mut color_cmd_map
-                } else {
-                    &mut alpha_cmd_map
-                };
+                    let cmd = cmd_map.entry(rendered.texture_index).or_insert_with(|| DrawCmd {
+                        image_id: self.glyph_textures[rendered.texture_index].image_id,
+                        quads: Vec::new(),
+                    });
 
-                let cmd = cmd_map.entry(rendered.texture_index).or_insert_with(|| DrawCmd {
-                    image_id: self.glyph_textures[rendered.texture_index].image_id,
-                    quads: Vec::new(),
-                });
+                    let mut q = Quad::default();
+                    let it = 1.0 / TEXTURE_SIZE as f32;
 
-                let mut q = Quad::default();
-                let it = 1.0 / TEXTURE_SIZE as f32;
+                    q.x0 = (position_x + glyph.x_int + rendered.offset_x - GLYPH_PADDING as i32) as f32;
+                    q.y0 = (position_y + run.line_y + glyph.y_int - rendered.offset_y - GLYPH_PADDING as i32) as f32;
+                    q.x1 = q.x0 + rendered.width as f32;
+                    q.y1 = q.y0 + rendered.height as f32;
 
-                q.x0 = (position_x + glyph.x_int + rendered.offset_x - GLYPH_PADDING as i32) as f32;
-                q.y0 = (position_y + run.line_y + glyph.y_int - rendered.offset_y - GLYPH_PADDING as i32) as f32;
-                q.x1 = q.x0 + rendered.width as f32;
-                q.y1 = q.y0 + rendered.height as f32;
+                    q.s0 = rendered.atlas_x as f32 * it;
+                    q.t0 = rendered.atlas_y as f32 * it;
+                    q.s1 = (rendered.atlas_x + rendered.width) as f32 * it;
+                    q.t1 = (rendered.atlas_y + rendered.height) as f32 * it;
 
-                q.s0 = rendered.atlas_x as f32 * it;
-                q.t0 = rendered.atlas_y as f32 * it;
-                q.s1 = (rendered.atlas_x + rendered.width) as f32 * it;
-                q.t1 = (rendered.atlas_y + rendered.height) as f32 * it;
-
-                cmd.quads.push(q);
+                    cmd.quads.push(q);
+                }
             }
         }
 
