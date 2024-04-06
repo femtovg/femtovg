772a077cb0c507216bbc9e8d80c0eb680374248b
Emil <11597870+Drewol@users.noreply.github.com>
2024-04-06 19:06:02 +0200

    Make Verb and PathIter public

--- src/lib.rs
+++ src/lib.rs
@@ -58,7 +58,7 @@ use paint::{GlyphTexture, PaintFlavor, StrokeSettings};
 
 mod path;
 use path::Convexity;
-pub use path::{Path, Solidity};
+pub use path::{Path, Solidity, Verb, PathIter};
 
 mod gradient_store;
 use gradient_store::GradientStore;
