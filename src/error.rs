991dd82b9f898f826a97a2073d0c29612d41b8a8
p11 <Krapohl.f@gmx.de>
2024-09-23 22:06:19 +0200

    Improved and added missing variant documentation for `ErrorKind` with help of AI

--- src/error.rs
+++ src/error.rs
@@ -5,25 +5,40 @@ use std::{
     io,
 };
 
-/// Enum with all possible canvas errors that could occur.
+/// Enum representing different types of errors that can occur in the canvas.
 #[derive(Debug)]
 #[non_exhaustive]
 pub enum ErrorKind {
+    /// An unknown error occurred.
     UnknownError,
+    /// A general error with a custom error message.
     GeneralError(String),
+    /// An error related to image loading (requires "image-loading" feature).
     #[cfg(feature = "image-loading")]
     ImageError(::image::ImageError),
+    /// An I/O error.
     IoError(io::Error),
+    /// An error occurred while parsing a font.
     FontParseError,
+    /// No font was found.
     NoFontFound,
+    /// An error occurred while extracting font information.
     FontInfoExtractionError,
+    /// The font size is too large for the font atlas.
     FontSizeTooLargeForAtlas,
+    /// An error occurred while compiling a shader.
     ShaderCompileError(String),
+    /// An error occurred while linking a shader.
     ShaderLinkError(String),
+    /// An error related to a render target.
     RenderTargetError(String),
+    /// The specified image ID was not found.
     ImageIdNotFound,
+    /// An error occurred while updating an image, as it is out of bounds.
     ImageUpdateOutOfBounds,
+    /// An error occurred while updating an image with a different format.
     ImageUpdateWithDifferentFormat,
+    /// The specified image format is not supported.
     UnsupportedImageFormat,
 }
 
