femtovg/femtovg

commit a1e215782a60df7e4fde9271fe7f95c134ac832f

Clean up glyph positioning
Unify the code for glyph positioning between atlas and direct rendering,
by providing an iterator to render_direct and render_atlas.

src/lib.rs
@@ -1385,10 +1385,20 @@ where
         let bitmap_glyphs = layout.has_bitmap_glyphs();
         let need_direct_rendering = text_settings.font_size > 92.0;
 
+        let glyphs = layout
+            .glyphs
+            .iter()
+            .filter(|shaped_glyph| !shaped_glyph.c.is_control())
+            .map(|shaped_glyph| {
+                let glyph_x = shaped_glyph.x - shaped_glyph.bearing_x;
+                let glyph_y = shaped_glyph.y + shaped_glyph.bearing_y;
+                (glyph_x, glyph_y, shaped_glyph.font_id, shaped_glyph.glyph_id)
+            });
+
         if need_direct_rendering && !bitmap_glyphs {
             text::render_direct(
                 self,
-                &layout,
+                glyphs,
                 &paint.flavor,
                 paint.shape_anti_alias,
                 &stroke,
@@ -1404,7 +1414,7 @@ where
             };
 
             let draw_commands =
-                atlas.render_atlas(self, &layout, text_settings.font_size, stroke.line_width, render_mode)?;
+                atlas.render_atlas(self, glyphs, text_settings.font_size, stroke.line_width, render_mode)?;
             self.draw_glyph_commands(draw_commands, paint, scale);
         }
 

src/text.rs
@@ -1,7 +1,6 @@
 use std::{
     borrow::Borrow,
     cell::RefCell,
-    collections::HashMap,
     convert::TryInto,
     ffi::OsStr,
     fs,
@@ -915,7 +914,7 @@ impl GlyphAtlas {
     pub(crate) fn render_atlas<T: Renderer>(
         &self,
         canvas: &mut Canvas<T>,
-        text_layout: &TextMetrics,
+        glyphs: impl IntoIterator<Item = (f32, f32, FontId, u16)>,
         font_size: f32,
         line_width: f32,
         mode: RenderMode,
@@ -931,22 +930,13 @@ impl GlyphAtlas {
 
         let initial_render_target = canvas.current_render_target;
 
-        for glyph in text_layout.glyphs.iter().filter(|g| !g.c.is_control()) {
-            let subpixel_location = crate::geometry::quantize(glyph.x.fract(), 0.1) * 10.0;
+        for (glyph_x, glyph_y, font_id, glyph_id) in glyphs.into_iter() {
+            let subpixel_location = crate::geometry::quantize(glyph_x.fract(), 0.1) * 10.0;
 
-            let id = RenderedGlyphId::new(
-                glyph.glyph_id,
-                glyph.font_id,
-                font_size,
-                line_width,
-                mode,
-                subpixel_location as u8,
-            );
+            let id = RenderedGlyphId::new(glyph_id, font_id, font_size, line_width, mode, subpixel_location as u8);
 
             if !self.rendered_glyphs.borrow().contains_key(&id) {
-                if let Some(glyph) =
-                    self.render_glyph(canvas, font_size, line_width, mode, glyph.font_id, glyph.glyph_id)?
-                {
+                if let Some(glyph) = self.render_glyph(canvas, font_size, line_width, mode, font_id, glyph_id)? {
                     self.rendered_glyphs.borrow_mut().insert(id, glyph);
                 } else {
                     continue;
@@ -977,11 +967,8 @@ impl GlyphAtlas {
 
                 let line_width_offset = if rendered.color_glyph { 0. } else { line_width_offset };
 
-                q.x0 = glyph.x.trunc() - line_width_offset - GLYPH_PADDING as f32;
-                q.y0 = (glyph.y + glyph.bearing_y).round()
-                    - rendered.bearing_y as f32
-                    - line_width_offset
-                    - GLYPH_PADDING as f32;
+                q.x0 = glyph_x.trunc() - line_width_offset - GLYPH_PADDING as f32;
+                q.y0 = glyph_y.round() - rendered.bearing_y as f32 - line_width_offset - GLYPH_PADDING as f32;
                 q.x1 = q.x0 + rendered.width as f32;
                 q.y1 = q.y0 + rendered.height as f32;
 
@@ -1260,7 +1247,7 @@ impl GlyphAtlas {
 
 pub fn render_direct<T: Renderer>(
     canvas: &mut Canvas<T>,
-    text_layout: &TextMetrics,
+    glyphs: impl IntoIterator<Item = (f32, f32, FontId, u16)>,
     paint_flavor: &PaintFlavor,
     anti_alias: bool,
     stroke: &StrokeSettings,
@@ -1269,19 +1256,16 @@ pub fn render_direct<T: Renderer>(
     invscale: f32,
 ) -> Result<(), ErrorKind> {
     let text_context = canvas.text_context.clone();
-    let text_context = text_context.borrow_mut();
+    let mut text_context = text_context.borrow_mut();
 
-    let mut face_cache: HashMap<FontId, rustybuzz::Face> = HashMap::default();
-
-    for glyph in text_layout.glyphs.iter().filter(|g| !g.c.is_control()) {
+    for (glyph_x, glyph_y, font_id, glyph_id) in glyphs.into_iter() {
         let (glyph_rendering, scale) = {
-            let font = text_context.font(glyph.font_id).ok_or(ErrorKind::NoFontFound)?;
-            let face = face_cache.entry(glyph.font_id).or_insert_with(|| font.face_ref());
+            let font = text_context.font_mut(font_id).ok_or(ErrorKind::NoFontFound)?;
+            let face = font.face_ref();
 
             let scale = font.scale(font_size);
 
-            let Some(glyph_rendering) = font.glyph_rendering_representation(&face, glyph.glyph_id, font_size as u16)
-            else {
+            let Some(glyph_rendering) = font.glyph_rendering_representation(&face, glyph_id, font_size as u16) else {
                 continue;
             };
 
@@ -1295,10 +1279,7 @@ pub fn render_direct<T: Renderer>(
             RenderMode::Stroke => stroke.line_width / scale,
         };
 
-        canvas.translate(
-            (glyph.x - glyph.bearing_x) * invscale,
-            (glyph.y + glyph.bearing_y) * invscale,
-        );
+        canvas.translate(glyph_x * invscale, glyph_y * invscale);
         canvas.scale(scale * invscale, -scale * invscale);
 
         match glyph_rendering {

commit 27f65dcc826edc2216d2e506c1566530bf52daf6

Fix regression with glyph positioning
Commit a1e215782a60df7e4fde9271fe7f95c134ac832f introduced the bug that
the bearing was added to all shaped glyphs, when previously it was
accounted for only in the render_direct case. But that was just the tip
of a big iceberg, which this patch attempts to melt down:

(minor clarification upfront: bearing x/y here is the same as xmin/ymin
in the bounding box of the glyph's outline - within the coordinate space
of the glyph)

- In the shaper (in `fn layout()` specifically), the `glyph.x` and
  `glyph.y` was computed so that it includes a bearing. This is not
  what's typically done and with the new fill_glyph_run/stroke_glyph_run
  APIs we should expect glyph coordinates for the glyph "box", excluding
  the bearing.
- Because the shaper used to add the bearing to the glyph, it had to be
  removed again when rendering the glyphs as path. The whole point of
  xmin/ymin is that it's just a computed value and the path knows very
  well by itself where to start. This was prematurely removed in
  a1e215782a60df7e4fde9271fe7f95c134ac832f, but remains removed.
- However, the bearing adjustment from
  a1e215782a60df7e4fde9271fe7f95c134ac832f in `fn draw_text()` was bogus
  and needs to be removed again.
- Finally, the only place where the bearing - when it's computed as
  xmin/ymin of the outline - is relevant, for the glyph atlas, where the
  bearing is excluded from the region in the atlas the glyph is rendered
  into (to save pixels), and where it's added again to the glyph screen
  coordinates when blitting from that texture to the screen. Note that
  the sign for y is inversed because the texture is y-flipped.

src/lib.rs
@@ -1418,8 +1418,8 @@ where
         {
             self.draw_glyph_run(
                 glyph_run.map(|shaped_glyph| PositionedGlyph {
-                    x: (shaped_glyph.x - shaped_glyph.bearing_x) * invscale,
-                    y: (shaped_glyph.y + shaped_glyph.bearing_y) * invscale,
+                    x: shaped_glyph.x * invscale,
+                    y: shaped_glyph.y * invscale,
                     glyph_id: shaped_glyph.glyph_id,
                 }),
                 &paint,

src/text.rs
@@ -119,6 +119,7 @@ pub struct RenderedGlyph {
     texture_index: usize,
     width: u32,
     height: u32,
+    bearing_x: i32,
     bearing_y: i32,
     atlas_x: u32,
     atlas_y: u32,
@@ -459,7 +460,7 @@ impl GlyphAtlas {
 
                 let line_width_offset = if rendered.color_glyph { 0. } else { line_width_offset };
 
-                q.x0 = glyph.x.trunc() - line_width_offset - GLYPH_PADDING as f32;
+                q.x0 = glyph.x.trunc() + rendered.bearing_x as f32 - line_width_offset - GLYPH_PADDING as f32;
                 q.y0 = glyph.y.round() - rendered.bearing_y as f32 - line_width_offset - GLYPH_PADDING as f32;
                 q.x1 = q.x0 + rendered.width as f32;
                 q.y1 = q.y0 + rendered.height as f32;
@@ -532,13 +533,15 @@ impl GlyphAtlas {
         canvas.save();
         canvas.reset();
 
+        let rendered_bearing_x = (glyph_metrics.bearing_x * scale).round();
         let rendered_bearing_y = (glyph_metrics.bearing_y * scale).round();
-        let x = dst_x as f32 - (glyph_metrics.bearing_x * scale) + line_width_offset + padding as f32;
+        let x = dst_x as f32 - rendered_bearing_x + line_width_offset + padding as f32;
         let y = TEXTURE_SIZE as f32 - dst_y as f32 - rendered_bearing_y - line_width_offset - padding as f32;
 
         let rendered_glyph = RenderedGlyph {
             width: width - 2 * GLYPH_MARGIN,
             height: height - 2 * GLYPH_MARGIN,
+            bearing_x: rendered_bearing_x as i32,
             bearing_y: rendered_bearing_y as i32,
             atlas_x: dst_x as u32 + GLYPH_MARGIN,
             atlas_y: dst_y as u32 + GLYPH_MARGIN,

src/text/textlayout.rs
@@ -25,8 +25,6 @@ pub struct ShapedGlyph {
     pub advance_y: f32,
     pub offset_x: f32,
     pub offset_y: f32,
-    pub bearing_x: f32,
-    pub bearing_y: f32,
 }
 
 #[derive(Clone, Debug, Default)]
@@ -445,15 +443,11 @@ fn shape_word(
                 advance_y: position.y_advance as f32 * scale,
                 offset_x: position.x_offset as f32 * scale,
                 offset_y: position.y_offset as f32 * scale,
-                bearing_x: 0.0,
-                bearing_y: 0.0,
             };
 
             if let Some(glyph) = font.glyph(&face, g.glyph_id) {
                 g.width = glyph.metrics.width * scale;
                 g.height = glyph.metrics.height * scale;
-                g.bearing_x = glyph.metrics.bearing_x * scale;
-                g.bearing_y = glyph.metrics.bearing_y * scale;
             }
 
             shaped_word.width += g.advance_x + letter_spacing;
@@ -516,8 +510,8 @@ fn layout(
     };
 
     for glyph in &mut res.glyphs {
-        glyph.x = cursor_x + glyph.offset_x + glyph.bearing_x;
-        glyph.y = (cursor_y + alignment_offset_y).round() + glyph.offset_y - glyph.bearing_y;
+        glyph.x = cursor_x + glyph.offset_x;
+        glyph.y = (cursor_y + alignment_offset_y).round() + glyph.offset_y;
 
         min_y = min_y.min(glyph.y);
         max_y = max_y.max(glyph.y + glyph.height);