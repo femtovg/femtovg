femtovg/femtovg

commit e70b95b8c1728a557ec3c081788cfd1f33538f1c

Clean up bitmap glyph special casing
Determine whether we're about to render bitmap glyphs solely based on
the glyphs on the fly, there's no need to precompute that.

src/lib.rs
@@ -1382,7 +1382,6 @@ where
 
         // TODO: Early out if text is outside the canvas bounds, or maybe even check for each character in layout.
 
-        let bitmap_glyphs = layout.has_bitmap_glyphs();
         let need_direct_rendering = text_settings.font_size > 92.0;
 
         let glyphs = layout
@@ -1395,7 +1394,19 @@ where
                 (glyph_x, glyph_y, shaped_glyph.font_id, shaped_glyph.glyph_id)
             });
 
-        if need_direct_rendering && !bitmap_glyphs {
+        let have_bitmap_glyphs = {
+            let text_context = self.text_context.clone();
+            let mut text_context = text_context.borrow_mut();
+
+            glyphs.clone().any(|(_, _, font_id, glyph_id)| {
+                text_context
+                    .font_mut(font_id)
+                    .and_then(|font| font.glyph(&font.face_ref(), glyph_id))
+                    .map(|glyph| glyph.path.is_none())
+                    .unwrap_or(false)
+            })
+        };
+        if need_direct_rendering && !have_bitmap_glyphs {
             text::render_direct(
                 self,
                 glyphs,
@@ -1407,7 +1418,7 @@ where
                 invscale,
             )?;
         } else {
-            let atlas = if bitmap_glyphs && need_direct_rendering {
+            let atlas = if have_bitmap_glyphs && need_direct_rendering {
                 self.ephemeral_glyph_atlas.get_or_insert_with(Default::default).clone()
             } else {
                 self.glyph_atlas.clone()

src/text.rs
@@ -147,7 +147,6 @@ pub struct ShapedGlyph {
     pub offset_y: f32,
     pub bearing_x: f32,
     pub bearing_y: f32,
-    pub bitmap_glyph: bool,
 }
 
 #[derive(Clone, Debug, Default)]
@@ -528,10 +527,6 @@ impl TextMetrics {
     pub fn height(&self) -> f32 {
         self.height
     }
-
-    pub(crate) fn has_bitmap_glyphs(&self) -> bool {
-        self.glyphs.iter().any(|g| g.bitmap_glyph)
-    }
 }
 
 // Shaper
@@ -777,7 +772,6 @@ fn shape_word(
                 offset_y: position.y_offset as f32 * scale,
                 bearing_x: 0.0,
                 bearing_y: 0.0,
-                bitmap_glyph: false,
             };
 
             if let Some(glyph) = font.glyph(&face, g.glyph_id) {
@@ -785,7 +779,6 @@ fn shape_word(
                 g.height = glyph.metrics.height * scale;
                 g.bearing_x = glyph.metrics.bearing_x * scale;
                 g.bearing_y = glyph.metrics.bearing_y * scale;
-                g.bitmap_glyph = glyph.path.is_none();
             }
 
             shaped_word.width += g.advance_x + letter_spacing;

commit 50c2072a960315d5ec212df121775902eacb9726

Fix regression in text rendering performance
Commit e70b95b8c1728a557ec3c081788cfd1f33538f1c introduced a filter on
the glyphs where for each glyph the ttf parser would be created.

Also, avoid the entire color glyph dance if no color glyphs were
collected.

Cargo.toml
@@ -1,7 +1,7 @@
 [package]
 name = "femtovg"
 description = "Antialiased 2D vector drawing library"
-version = "0.19.2"
+version = "0.19.3"
 license = "MIT OR Apache-2.0"
 readme = "README.md"
 authors = [

src/lib.rs
@@ -1466,7 +1466,7 @@ where
         let non_color_glyphs = glyphs_it
             .filter(|glyph| {
                 if font
-                    .glyph(&font.face_ref(), glyph.glyph_id)
+                    .glyph(&font_face, glyph.glyph_id)
                     .map(|glyph| glyph.path.is_none())
                     .unwrap_or(false)
                 {
@@ -1504,27 +1504,29 @@ where
             )?
         };
 
-        let color_commands = {
-            let atlas = if need_direct_rendering {
-                self.ephemeral_glyph_atlas.get_or_insert_with(Default::default).clone()
-            } else {
-                self.glyph_atlas.clone()
+        if !color_glyphs.is_empty() {
+            let color_commands = {
+                let atlas = if need_direct_rendering {
+                    self.ephemeral_glyph_atlas.get_or_insert_with(Default::default).clone()
+                } else {
+                    self.glyph_atlas.clone()
+                };
+
+                atlas.render_atlas(
+                    self,
+                    font_id,
+                    font,
+                    &font_face,
+                    color_glyphs.into_iter(),
+                    paint.text.font_size,
+                    paint.stroke.line_width,
+                    render_mode,
+                )?
             };
 
-            atlas.render_atlas(
-                self,
-                font_id,
-                font,
-                &font_face,
-                color_glyphs.into_iter(),
-                paint.text.font_size,
-                paint.stroke.line_width,
-                render_mode,
-            )?
-        };
-
-        draw_commands.alpha_glyphs.extend(color_commands.alpha_glyphs);
-        draw_commands.color_glyphs.extend(color_commands.color_glyphs);
+            draw_commands.alpha_glyphs.extend(color_commands.alpha_glyphs);
+            draw_commands.color_glyphs.extend(color_commands.color_glyphs);
+        }
 
         self.draw_glyph_commands(draw_commands, &paint);
 